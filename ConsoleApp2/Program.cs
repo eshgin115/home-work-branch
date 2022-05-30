82fc7717febe8acb2b9c27bbfb02dd39dfd3794e
unknown <you@example.com>
2022-05-30 05:41:08 -0700

    yeni branch2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("BUDCE PROGRAMMI");
+
+
+Console.WriteLine();
+Console.WriteLine("BUDGET");
+
+
+
+int BT = 250;
+Console.WriteLine(BT);
+Console.WriteLine();
+
+
+
+Console.WriteLine("MN");
+
+float name1 = 0.2f;
+float MN = name1 * BT;
+Console.WriteLine(MN);  
+Console.WriteLine();
+
+Console.WriteLine("TN");
+float name2 = 0.1f;
+float TN = name2 * BT;
+
+Console.WriteLine( TN);
+
+
+Console.WriteLine();
+
+
+Console.WriteLine("BALANCE BUDGET");
+
+Console.WriteLine(BT - MN - TN);
+
