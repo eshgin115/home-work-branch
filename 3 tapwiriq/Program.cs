f8eb2cc77979ad550dc5f010628b905cd4d75ede
unknown <you@example.com>
2022-05-30 05:43:42 -0700

    teze branch 3

---
+++ 3 tapwiriq/Program.cs
@@ -0,0 +1,59 @@
+﻿
+
+
+Console.WriteLine("KARGO PROGRAMM");
+
+Console.WriteLine();
+Console.WriteLine("FIRMA");
+Console.WriteLine("adidas");
+Console.WriteLine("number of sneaker");
+int adidas = 4;
+Console.WriteLine(adidas);
+Console.WriteLine("price of one sneaker");
+int price = 300;
+
+Console.WriteLine(price);
+Console.WriteLine("topprice");
+int topprice = adidas * price;
+
+Console.WriteLine(topprice);
+
+
+Console.WriteLine();
+Console.WriteLine("FIRMA2");
+Console.WriteLine("nike");
+Console.WriteLine("number of sneaker2");
+int nike = 1;
+Console.WriteLine(nike);
+Console.WriteLine("price of one sneaker2");
+int price2 = 200;
+Console.WriteLine(price2);
+Console.WriteLine("topprice2");
+int topprice2 =nike * price2;
+
+Console.WriteLine(topprice2);
+
+Console.WriteLine();
+Console.WriteLine("FIRMA3");
+Console.WriteLine("gucci");
+Console.WriteLine("number of sneaker3");
+int gucci = 5;
+Console.WriteLine(nike);
+Console.WriteLine("price of one sneaker3");
+int price3 = 500;
+Console.WriteLine(price3);
+Console.WriteLine("topprice3");
+int topprice3 = gucci * price3;
+
+Console.WriteLine(topprice3);
+
+Console.WriteLine();
+Console.WriteLine("final spent");
+int finalspent = topprice + topprice2 + topprice3;
+
+Console.WriteLine(finalspent);
+
+
+
+
+
